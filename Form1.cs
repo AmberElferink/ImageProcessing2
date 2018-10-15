4b9580725675c8cb6750a08c7ac8d1ef14d67125
Amber Elferink <a.s.elferink@students.uu.nl>
2018-10-15 16:42:00 +0200

    TraceBoundaries poging

--- Form1.cs
+++ Form1.cs
@@ -62,13 +62,21 @@ namespace INFOIBV
 
         private void applyButton_Click(object sender, EventArgs e)
         {
+            resetForApply();
+
             if (InputImage1 == null)
             {
-                //MessageBox2.Text = "Please Load an image first.";
+                MessageBox2.Text = "Please Load an image first.";
                 return;
             }
+            if (BoundaryRadio.Checked)
+            {
+                Point[] boundary = TraceBoundary();
+                kernelInput.Text = WritePointArr(boundary);
+                BoundaryToOutput(boundary);
+            }
 
-            resetForApply();
+
 
             /*
             //==========================================================================================
@@ -101,7 +109,205 @@ namespace INFOIBV
             outputBox1.Image = (Image)OutputImage1;                         // Display output image
             progressBar.Visible = false;                                    // Hide progress bar
         }
+
+
+        String WritePointArr(Point[] points)
+        {
+            String output = "{";
+            for( int i = 0; i < points.Length; i++)
+            {
+                output = output + "(" + points[i].X + "," + points[i].Y + "), ";
+            }
+            output += "}";
+            return output;
+        }
+
+
+        void BoundaryToOutput(Point[] points)
+        {
+            for(int x = 0; x < InputImage1.Width; x++)
+                for (int y = 0; y < InputImage1.Height; y++)
+                {
+                    if (points.Contains(new Point(x, y)))
+                        OutputImage1.SetPixel(x, y, Color.FromArgb(255, 0, 0, 0));
+                    else
+                        OutputImage1.SetPixel(x, y, Color.FromArgb(255, 255, 255, 255));
+                }
+
+            pictureBox2.Image = (Image) OutputImage1;                         // Display output image
+            progressBar.Visible = false;             
+      
+        }
+
+
+
+        Point[] TraceBoundary()
+        {
+            Color backGrC = Color.FromArgb(255, 0, 0, 0);
+            //Color foreGrC = Color.FromArgb(255, 255, 255, 255);
+
+            Color previousColor = backGrC;
+
+            List<Point> returnValue = new List<Point>();
+
+             for( int v = 0; v < InputImage1.Height; v++)
+                for (int u = 0; u < InputImage1.Width; u++) //x moet 'snelst' doorlopen
+                {
+                        Color currentColor = Image[u, v];
+                        if (previousColor == backGrC && currentColor != backGrC)
+                           returnValue = TransFgBg(backGrC, returnValue, u, v);
+                }
+           
+                return returnValue.ToArray();
+        }
+
+
+
+        /// <summary>
+        /// Handles the case that there is a transition from background to foreground in the image, and traces the figure found.
+        /// </summary>
+        /// <param name="backgrC">background color</param>
+        /// <param name="u"></param>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        List<Point> TransFgBg(Color backgrC, List<Point> ContourPixels, int u, int v)
+        {
+
+            if (isContourPix(backgrC, u, v))
+            {
+                if (Image[u, v] != backgrC)
+                {
+                    ContourPixels.Add(new Point(u, v));
+
+                    //This is N8 chain code, for N4 only consider the 4 pixels straight up, below, left and right
+                    for (int x = -1; x <= 1; x++) //kijk van rechtsonder naar linksboven, dan loop je minder snel terug.
+                        for (int y = -1; y <= 1; y++)
+                        {
+                            if (u + x >= 0 && v + y >= 0 && u + x < InputImage1.Width && v + y < InputImage1.Height)
+                                if (!ContourPixels.Contains(new Point(u + x, v + y)))
+                                {
+                                    TransFgBg(backgrC, ContourPixels, u + x, v + y);
+                                }
+
+                        }
+                }
+
+            }
+            //If all edge pixels are traced, return the complete list of contourpixels 
+               return ContourPixels;
+        }
+
+        Boolean isContourPix(Color backgrC, int u, int v)
+        {
+            for(int x = -1; x <= 1; x++ ) //check if neighbouringpixels are background
+                for (int y = -1; y <= 1; y++)
+                {
+                    if(u+x >= 0 && v+y >= 0 && u+x < InputImage1.Width && v+y < InputImage1.Height)
+                    if (Image[u + x, v + y] == backgrC)
+                        return true;
+                }
+
+            return false;
+        }
+
+
+
+
+
+
+
         
+
+        void ColorShapes()
+        {
+            bool useN8Seq = true;
+
+            Color foreGrC = Color.FromArgb(255, 255, 255, 255);
+            Color backGrC = Color.FromArgb(255, 0, 0, 0);
+
+
+
+            
+
+            int[,] N4Seq = new int[,] { { -1, 1, -1 }, { 1, -1, -1 }, { -1, -1, -1 } }; //alleen boven en links vd de middelste
+            int[,] N8Seq = new int[,] { { 1, 1, 1 }, { 1, -1, -1 }, { -1, -1, -1 } }; //links vd middelste en de bovenste rij
+            int[,] usedSeq = new int[3, 3];
+            if (useN8Seq)
+                usedSeq = N8Seq;
+            else
+                usedSeq = N4Seq;
+
+            for (int v = 1; v < InputImage1.Size.Height - 1; v++) //from 1 until height -1 to fit 3x3 usedSeq on the current pixel
+                for (int u = 1; u < InputImage1.Size.Width - 1; u++)
+                {
+                    if (Image[u, v] == foreGrC)
+                    {
+                        Color Neighbour = CheckNeighbPixels(usedSeq, u, v, backGrC);
+                        if (Neighbour != Color.FromArgb(-1, -1, -1, -1))
+                            Image[u, v] = Neighbour;
+                        else
+                            addNewColorTag();
+
+                    }
+                }
+
+            newImage = Image;
+            toOutputBitmap();
+        }
+
+
+        void addNewColorTag()
+        {
+            //nog definieren dat hij Image[u,v] hier nu een nieuwe niet gebruikte kleur geeft.
+
+            //EN: nadat de hele image is doorlopen. nog de collision structuren bij elkaar voegen.
+        }
+
+
+
+
+
+
+
+
+        Stack<Point> collisionPixels = new Stack<Point>();
+        /// <summary>
+        ///  if this returns the backGrC, it has no previously processed foregr pixels. If there is a processed pixel, it gives back the first processed color it encounters.
+        /// </summary>
+        /// <returns></returns>
+        Color CheckNeighbPixels(int[,] usedSeq, int u, int v, Color backGrC)
+        {
+            int processedPix = 0;
+            Color returnNeighb = Color.FromArgb(-1,-1,-1,-1); //not an existing color
+
+            for (int x = 0; x < usedSeq.GetLength(0); x++)
+            {
+                for (int y = 0; y < usedSeq.GetLength(1); y++)
+                {
+
+                    if (usedSeq[x, y] == 1) //if this neighbouring pixel must be checked,
+                    {
+                        Color neighbour = Image[u - 1 + x, v - 1 + y];
+                        if (neighbour != backGrC)
+                        {
+                            if (processedPix == 0)
+                            {
+                                returnNeighb = neighbour;
+                                continue;
+                            }
+                            else if (returnNeighb != neighbour) //collision is found
+                            {
+                                collisionPixels.Push(new Point(u, v)); //save collision to merge the touching structures later.
+                                returnNeighb = neighbour;
+                            }
+                        }
+                    }
+                }
+            }
+
+              return returnNeighb;
+        }
+
         private void saveButton_Click(object sender, EventArgs e)
         {
             if (OutputImage1 == null) return;                                // Get out if no output image
